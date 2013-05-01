4a6a6da587815578cf2b1ddfe25ebb74b45a0202
lalo73 <leandro.gz73@gmail.com>
2013-05-01 14:36:17 -0300

    add behavior

--- tp_concurrente/src/concubattles/Game.java
+++ tp_concurrente/src/concubattles/Game.java
@@ -10,8 +10,20 @@ public class Game {
 		return next;
 	}
 	
+	public Castle createCastle(){
+		return new Castle(new Channel<String>(this.getNextIn()));
+	}
+	
+	public City createCity(){
+		return new City(new Channel<String>(this.getNextIn()));
+	}
+	
+	private Way createway(){
+		return new Way(new Channel<String>(this.getNextIn()));
+	}
+	
 	public void connect(Place place1, Place place2){
-		Way way = new Way(new Channel<String>(this.getNextIn()));
+		Way way = this.createway();
 		place1.getRoads().add(way);
 		place2.getRoads().add(way);
 		way.getRoads().add(place1);
@@ -20,7 +32,14 @@ public class Game {
 	
 	public static void main(String[] args) {
 		
-		
+		Game game = new Game();
+		Castle team1 = game.createCastle();
+		Castle team2 = game.createCastle();
+		City city1 = game.createCity();
+		City city2 = game.createCity();
+		game.connect(team1, city1);
+		game.connect(city1, city2);
+		game.connect(city2, team2);			
 	}
 
 }
