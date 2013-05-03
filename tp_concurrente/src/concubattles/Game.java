a7e5523fa2719f4bb33575d353534883b2fcb4c8
lalo73 <leandro.gz73@gmail.com>
2013-05-02 22:48:56 -0300

    Added Concurrent folder

--- tp_concurrente/src/concubattles/Game.java
+++ tp_concurrente/src/concubattles/Game.java
@@ -5,11 +5,19 @@ import ar.edu.unq.tpi.pconc.Channel;
 public class Game {
 	
 	private int count;
+	private Channel<String> input;
+	private Channel<String> output;
 	
 	public int getNextIn(){
 		int next = this.count;
 		this.count++;
 		return next;
+	}
+	
+	public Game(Channel<String> input, Channel<String> output){
+		this.count = 5;
+		this.input = input;
+		this.output = output;
 	}
 	
 	public Castle createCastle(){
@@ -32,18 +40,24 @@ public class Game {
 		way.getRoads().add(place2);
 	}
 	
+	public void receive(){
+		System.out.println(this.input.receive());
+	}
+	
 	public static void main(String[] args) {
 		
-		Game game = new Game();
-		Castle team1 = game.createCastle();
-		Castle team2 = game.createCastle();
-		City city1 = game.createCity();
-		City city2 = game.createCity();
-		game.connect(team1, city1);
-		game.connect(city1, city2);
-		game.connect(city2, team2);
-		team1.createSoldier();
-		team2.createSoldier();
+		Game game = new Game(new Channel<String>(1002), new Channel<String>(1001));
+		
+		game.receive();
+//		Castle team1 = game.createCastle();
+//		Castle team2 = game.createCastle();
+//		City city1 = game.createCity();
+//		City city2 = game.createCity();
+//		game.connect(team1, city1);
+//		game.connect(city1, city2);
+//		game.connect(city2, team2);
+//		team1.createSoldier();
+//		team2.createSoldier();
 	}
 
 }
